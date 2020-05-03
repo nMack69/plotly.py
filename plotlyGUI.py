5d2a479aa8af4eb426be1dfdf59a9ea9df26c992
Nathan Mack <mack.62@wright.edu>
2020-05-02 22:57:32 -0400

    Added a single 3D scatter plot plotly function into PlotlyGUI and modified PlotlyGUI code to pass inputted integer values directly into the Plotly function. This allows the GUI to be used as a standalone plotly environment for 3D scatter plots, showing the resulting plot in a new browser tab

--- plotlyGUI.py
+++ plotlyGUI.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+import plotly.graph_objects as go
+import numpy as np
 from tkinter import *
 root = Tk()
 
@@ -18,13 +19,41 @@ for line in f:
 
 tkvar.set('2D-Histogram.md')
 
+# Enter space separated INTEGER values into 'entry 1' and 'entry 2' fields on PlotlyGUI
+# ex, 1 2 3 4 5 6 etc.
+# Only 3D scatter plots supported at the moment
+def scatterPlot():
+	# Helix equation
+	t = np.linspace(0, 20, 100)
+	x, y, z = list(map(int, entry1.get().split())), list(map(int, entry2.get().split())), t
+
+	fig = go.Figure(data=[go.Scatter3d(
+		x=x,
+		y=y,
+		z=z,
+		mode='markers',
+		marker=dict(
+			size=12,
+			color=z,                # set color to an array/list of desired values
+			colorscale='Viridis',   # choose a colorscale
+			opacity=0.8
+		)
+	)])
+
+	# tight layout
+	fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+	fig.show()
+
+
 def change_dropdown(*args):
 	tkvar.trace('w', change_dropdown)
 
+# handler for button and text fields
 def clickRun():
 	print(tkvar.get())
 	print(entry1.get())
 	print(entry2.get())
+	scatterPlot()
 
 buttonRun = Button(mainFrame, text="Run", command=clickRun)
 buttonRun.grid(row = 5, column = 3)
@@ -48,5 +77,12 @@ canvas1.pack()
 entry1 = Entry(root)
 canvas1.create_window(200, 140, window=entry1)
 '''
+
 root.mainloop()
 f.close()
+
+
+
+
+
+
