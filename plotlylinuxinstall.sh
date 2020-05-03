ff2f8b6da59f4b318e976513d111933e7c81cd55
ngriff43 <cincysharks43@gmail.com>
2020-05-03 17:45:49 -0400

    Updated Linux script file for automated process of installing the development of plotly.py. Added the installation commands for the requirement texts. Added installation of plotly package chart-studio. Added installation of plotly package plotly-geo.

--- plotlylinuxinstall.sh
+++ plotlylinuxinstall.sh
@@ -6,3 +6,13 @@ source .bashrc
 #Creates virtual environment
 conda create -n plotly-dev python -y
 conda activate plotly-dev
+#install requirements
+echo Installing text requirements
+pip install -r packages/python/plotly/requirements.txt
+pip install -r packages/python/plotly/optional-requirements.txt
+#Installation of plotly packages
+echo Installing editable plotly packages
+pip install -e packages/python/plotly/
+pip install -e packages/python/chart-studio/
+pip install -e packages/python/plotly-geo/
+echo Installing editable plotly packages completed
